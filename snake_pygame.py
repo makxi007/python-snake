08a6b21357cf0d18bd32e872750640200991c118
makxi007 <Mtolstov_2000@mail.ru>
2020-11-23 08:14:51 +0600

    Snake pygame3.0

--- snake_pygame.py
+++ snake_pygame.py
@@ -6,28 +6,36 @@ import random
 # Snake 
 	# make a grid +
 	# make a cube +
-	# make a moving Cube
+	# make a moving Cube +
 	# add random Cube
 	# make Snake be bigger when eat Cube
 	# add score
 class Cube:
+	
 	def __init__(self, posx, posy, color=COLOR):
 		self.posx = posx
 		self.posy = posy
 		self.pos = (self.posx, self.posy)
 		self.color = color
 
+		self.dirnx = 0
+		self.dirny = 1
+
 	def move(self, dirnx, dirny):
-		self.posx += dirnx 
-		self.posy += dirny
+		self.dirnx = dirnx
+		self.dirny = dirny
+
+		self.posx += self.dirnx 
+		self.posy += self.dirny
 
 	def draw(self, window):
-		pygame.draw.rect(window, COLOR ,(DIST_BTN_ROWS * self.posx, DIST_BTN_ROWS * self.posy, DIST_BTN_ROWS, DIST_BTN_ROWS))		
+		distance_btn_rows = SIZE // ROWS
+		pygame.draw.rect(window, GREEN ,(DIST_BTN_ROWS * self.posx, DIST_BTN_ROWS * self.posy, DIST_BTN_ROWS, DIST_BTN_ROWS))		
 
 class Snake:
 	body = []
 	turns = {}
-	def __init__(self, posx, posy, color=COLOR):
+	def __init__(self, posx, posy, color=GREEN):
 		self.posx = posx
 		self.posy = posy
 		self.dirnx = 0
@@ -48,33 +56,70 @@ class Snake:
 				self.dirnx = -1
 				self.dirny = 0
 				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+				self.add_cube()
 
 			if ( keys[pygame.K_RIGHT] ):
 				self.dirnx = 1
 				self.dirny = 0
+				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 				
 
 			if ( keys[pygame.K_UP] ):
 				self.dirny = -1
 				self.dirnx = 0
+				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 			if ( keys[pygame.K_DOWN] ):
 				self.dirny = 1
 				self.dirnx = 0
+				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 			
 
-			for i, c in enumerate(self.body):
-				p_x = c.posx
-				p_y = c.posy
-				print(p_x)
-				print(p_y)
-				print(self.turns)
+		for i, c in enumerate(self.body):
+			p = c.pos[:]
+			if p in self.turns:
+				turn = self.turns[p]
+				c.move(turn[0], turn[1])
+				if (i == len(self.body) - 1) == 1:
+					self.turns.pop(p)
 
-		self.head.move(self.dirnx, self.dirny)
+			else:
+				if c.dirnx == 1 and c.posx >= ROWS - 1: 
+					c.posx = 0
+					c.posy = c.posy
+					
+				elif c.dirnx == -1 and c.posx <= 0:
+					c.posx = ROWS - 1
+					c.posy = c.posy
+					
+				elif c.dirny == 1 and c.posy >= ROWS - 1:
+					c.posx = c.posx
+					c.posy = 0
+					
+				elif c.dirny == -1 and c.posy <= 0:
+					c.posx = c.posx
+					c.posy = ROWS - 1
+					
+				else:
+					c.move(c.dirnx, c.dirny)
+					
 
 	def draw(self, window):
 		self.head.draw(window)
+
+	def add_cube(self):
+		tail = self.body[-1]
+		tail_x, tail_y = tail.dirnx, tail.dirny
+
+		if tail_x == 1 and tail_y == 0:
+			self.body.append(Cube(tail.posx + 1, tail.posy, color=COLOR)) 
+
+		self.body[-1].dirnx = tail_x
+		self.body[-1].dirny = tail_y
+		for i in self.body:
+			print((i.posx, i.posy))
+		
 
 def draw_grid(window):
 	x = 0
@@ -86,17 +131,19 @@ def draw_grid(window):
 		pygame.draw.line(window, WHITE, (0,y), (SIZE, y))
 
 def redraw(window):
-	global snake
+	global snake, cube
 	window.fill(BLACK)
 	snake.draw(window)
+	cube.draw(window)
 	draw_grid(window)
 
 	pygame.display.update()
 
 
 def main():
-	global snake
+	global snake, cube
 	snake = Snake(15, 15)
+	cube = Cube(10, 10, color=(200, 200, 200))
 	window = pygame.display.set_mode((500, 500))
 
 	play = True
