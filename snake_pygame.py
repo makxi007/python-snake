a90a3410e910a89b2dd753c132f3ab5fdaa87786
makxi007 <Mtolstov_2000@mail.ru>
2020-11-25 13:26:10 +0600

    Pygame snake6.0

--- snake_pygame.py
+++ snake_pygame.py
@@ -10,6 +10,15 @@ import random
 	# add random Cube +
 	# make Snake be bigger when eat Cube + 
 	# add score
+	# add pause quit buttons
+	# add play again button
+	# add smash area
+	# add pause area
+	# make reset of the game:
+	# https://www.techwithtim.net/tutorials/game-development-with-python/snake-pygame/tutorial-4/
+
+pygame.init()
+
 class Cube:
 	
 	def __init__(self, posx, posy, color=GREEN):
@@ -30,7 +39,6 @@ class Cube:
 		self.pos = (self.posx, self.posy)
 
 	def draw(self, window):
-		distance_btn_rows = SIZE // ROWS
 		pygame.draw.rect(window, self.color ,(DIST_BTN_ROWS * self.posx, DIST_BTN_ROWS * self.posy, DIST_BTN_ROWS, DIST_BTN_ROWS))		
 
 class Snake:
@@ -46,6 +54,15 @@ class Snake:
 		self.body.append(self.head)
 		self.pos = (self.posx, self.posy)
 
+	def reset(self, pos):
+		self.head = Cube(pos[0], pos[1])
+		self.body = []
+		self.body.append(self.head)
+		self.turns = {}
+		self.dirnx = 0
+		self.dirny = -1
+
+
 	def move(self):
 		for event in pygame.event.get():
 			if (event.type == pygame.QUIT):
@@ -53,29 +70,26 @@ class Snake:
 
 			keys = pygame.key.get_pressed()
 
-			if ( keys[pygame.K_LEFT] ):
+			if (keys[pygame.K_a]):
 				self.dirnx = -1
 				self.dirny = 0
 				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-			if ( keys[pygame.K_RIGHT] ):
+			if (keys[pygame.K_d]):
 				self.dirnx = 1
 				self.dirny = 0
 				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 				
 
-			if ( keys[pygame.K_UP] ):
+			if (keys[pygame.K_w]):
 				self.dirny = -1
 				self.dirnx = 0
 				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-			if ( keys[pygame.K_DOWN] ):
+			if (keys[pygame.K_s]):
 				self.dirny = 1
 				self.dirnx = 0
 				self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-
-			
 
 		for i, c in enumerate(self.body):
 			p = c.pos[:]
@@ -110,11 +124,6 @@ class Snake:
 		for c in self.body:
 			c.draw(window)
 
-		for c in range(1, len(self.body)):
-			if ( self.head.posx == self.body[c].posx and self.head.posy == self.body[c].posy):
-				gameEvent().smash(True)
-				print("Smash!")
-
 	def add_cube(self):
 		tail = self.body[-1]
 		tail_x, tail_y = tail.dirnx, tail.dirny
@@ -130,7 +139,63 @@ class Snake:
 
 		self.body[-1].dirnx = tail_x
 		self.body[-1].dirny = tail_y
-		
+
+class gameEvent:
+	def reset_game(self):
+		global intro, snake
+		print("Reset")
+		intro = False
+		snake.reset((15,15))
+
+	def game_intro(self, window, msg):
+		global intro
+		intro = True
+		clock = pygame.time.Clock()
+		while (intro == True):
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+
+				keys = pygame.key.get_pressed()
+				#Press w to continue the game
+				if ( keys[pygame.K_t] ):
+				 	intro = False
+				 	break
+				
+			self.button(window,msg,150,100,100,100,PURPLE,DARK_PURPLE, self.reset_game)
+
+			pygame.display.update()
+			clock.tick(15)
+
+	def smash(self, window, is_smashed):
+		if (is_smashed == True):
+			while (is_smashed):
+				for event in pygame.event.get():
+					if (event.type == pygame.QUIT):
+						pygame.quit()
+				
+				self.button(window, "You was smashed!",150,100, 200,100,RED, DARK_RED)
+				self.button(window, "Play again!",200, 400, 100,50, GREEN, LIME_GREEN, self.reset_game)
+
+				pygame.display.update()
+
+	def text_objects(self, text, font):
+		text_surface = font.render(text, True, WHITE)
+		return text_surface, text_surface.get_rect()
+
+	def button(self,win, msg, x, y, w, h, ic, ac, action=None):
+		mouse = pygame.mouse.get_pos()
+		click = pygame.mouse.get_pressed()
+		if (x+w > mouse[0] > x and y+w > mouse[1] > y):
+			pygame.draw.rect(win, ic, (x,y,w,h))
+			if (click[0] == 1 and action != None):
+				action()
+		else:
+			pygame.draw.rect(win, ac, (x,y,w,h))
+		small_text = pygame.font.SysFont("Arial", 20)
+		text_surface, text_rect = self.text_objects(msg, small_text)
+		text_rect.center = ( (x+(w/2), (y+(h/2) )))
+		win.blit(text_surface, text_rect)
 
 def randomCube(item):
 	position = item.body
@@ -144,45 +209,7 @@ def randomCube(item):
 		else:
 			break
 	return (x, y)
-class gameEvent:
-	def pause(self, window, pause):
-		while pause:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
 
-				keys = pygame.key.get_pressed()
-				#Press w to continue the game
-				if ( keys[pygame.K_w] ):
-				 	pause = False
-				 	break
-				
-			self.button(window,"PAUSE",100,100,100,100,PURPLE,DARK_PURPLE)
-
-			pygame.display.update()
-
-
-	def smash(self, is_smashed):
-		while is_smashed:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-
-	def text_objects(self, text, font):
-		text_surface = font.render(text, True, WHITE)
-		return text_surface, text_surface.get_rect()
-
-	def button(self,win, msg, x, y, w, h, ic, ac, action=None):
-		mouse = pygame.mouse.get_pos()
-		click = pygame.mouse.get_pressed()
-		if (x+w > mouse[0] > x and y+h > mouse[0] > h):
-			pygame.draw.rect(win, ic, (x,y,w,h))
-		else:
-			pygame.draw.rect(win, ac, (x,y,w,h))
-		small_text = pygame.font.SysFont("NerkoOne-Regular", 20)
-		text_surface, text_rect = self.text_objects(msg, small_text)
-		text_rect.center = ( (x+(w/2), (y+(h/2) )))
-		window.blit(text_surface, text_rect)
 
 def draw_grid(window):
 	x = 0
@@ -202,50 +229,54 @@ def redraw(window):
 	draw_grid(window)
 	pygame.display.update()
 
-def main():
-	global snake, randcube
-	snake = Snake(15, 15)
-	randcube = Cube(10, 10, color=RED)
-	game_event = gameEvent()
-	window = pygame.display.set_mode((500, 500))
-
+def game_loop():
+	global snake, randcube, game_event, game_intro, window, play
 	play = True
-	pause = False
 
 	clock = pygame.time.Clock()
-	while play:
+	while (play):
 		
 		pygame.time.delay(50)
 		clock.tick(10)
 
-		if ( snake.body[0].posx == randcube.posx and snake.body[0].posy == randcube.posy ):
+		if (snake.body[0].posx == randcube.posx and snake.body[0].posy == randcube.posy):
 			snake.add_cube()
 			pos = randomCube(snake)
 			randcube = Cube(pos[0], pos[1], color=RED)
-			print(pos)
-
+		
 		snake.move()
 
 		keys = pygame.key.get_pressed()
-
-
 		#Press q to pause the game
-		if ( keys[pygame.K_q] ):
+		if (keys[pygame.K_r]):
 			pause = True
 			#Press w to continue the game
-			game_event.pause(window, pause)
+			game_event.game_intro(window, "Pause")
 
+		#Smash!
+		if (keys[pygame.K_y]):
+			game_event.smash(window, True)
 
-		for c in range(1, len(snake.body)):
-			if ( snake.head.posx == snake.body[c].posx and snake.head.posy == snake.body[c].posy ):
-				game_event.smash(True)
-		
+		for c in range(len(snake.body) ):
+			print(c)
 
-			
-
-					
+			check_ifx = snake.body[c].posx in list(map(lambda z:z.posx, snake.body[c+1:]))
+			check_ify = snake.body[c].posy in list(map(lambda z:z.posy, snake.body[c+1:]))
+			if (check_ifx and check_ify):
+				game_event.game_intro(window, "Smashed!")
+				break
 
 		redraw(window)
 
+def main():
+	global snake, randcube, game_event, window
+	snake = Snake(15, 15)
+	randcube = Cube(10, 10, color=RED)
+	game_event = gameEvent()
+
+	window = pygame.display.set_mode((500, 500))
+	
+	game_loop()
+	
 if __name__ == "__main__":
 	main()
